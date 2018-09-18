f144181e7e66bf6d483910e41f9e8933e5bf5a4a
GDD <844768109@qq.com>
2018-09-18 16:59:42 +0800

    【mdf】推送环境接入

--- LXCoach/LXCoach/AppDelegate.m
+++ LXCoach/LXCoach/AppDelegate.m
@@ -9,9 +9,9 @@
 #import "AppDelegate.h"
 #import "LXNavigationController.h"
 #import "LXLoginController.h"
-#import "LXTabBarController.h"
+#import <QQ_XGPush/XGPush.h>
 
-@interface AppDelegate ()
+@interface AppDelegate ()<XGPushDelegate>
 
 @end
 
@@ -26,12 +26,75 @@
     LXNavigationController *navc = [[LXNavigationController alloc] initWithRootViewController:login];
     self.window.rootViewController = navc;
     
-//    LXTabBarController *tabBarController = [[LXTabBarController alloc]init];
-//    self.window.rootViewController = tabBarController;
-    [self.window makeKeyAndVisible];
+    // debug
+    [[XGPush defaultManager] setEnableDebug:YES];
+    //调用信鸽的初始化方法
+    [[XGPush defaultManager] startXGWithAppID:0 appKey:@"" delegate:self];
     
+
+
+    [self.window makeKeyAndVisible];
     return YES;
 }
+
+/**
+ 收到推送的回调
+ @param application  UIApplication 实例
+ @param userInfo 推送时指定的参数
+ @param completionHandler 完成回调
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    [[XGPush defaultManager] reportXGNotificationInfo:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+
+// iOS 10 新增回调 API
+// App 用户点击通知
+// App 用户选择通知中的行为
+// App 用户在通知中心清除消息
+// 无论本地推送还是远程推送都会走这个回调
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >=     __IPHONE_10_0
+- (void)xgPushUserNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
+{
+    [[XGPush defaultManager] reportXGNotificationResponse:response];
+    completionHandler();
+}
+
+// App 在前台弹通知需要调用这个接口
+- (void)xgPushUserNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+{
+    [[XGPush defaultManager] reportXGNotificationInfo:notification.request.content.userInfo];
+    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+    
+}
+#endif
+
+
+#pragma mark - XGPushDelegate
+
+/**
+ @brief 监控信鸽推送服务地启动情况
+ 
+ @param isSuccess 信鸽推送是否启动成功
+ @param error 信鸽推送启动错误的信息
+ */
+- (void)xgPushDidFinishStart:(BOOL)isSuccess error:(nullable NSError *)error {
+    
+}
+
+/**
+ @brief 向信鸽服务器注册设备token的回调
+ 
+ @param deviceToken 当前设备的token
+ @param error 错误信息
+ @note 当前的token已经注册过之后，将不会再调用此方法
+ */
+- (void)xgPushDidRegisteredDeviceToken:(nullable NSString *)deviceToken error:(nullable NSError *)error {
+    
+}
+
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
